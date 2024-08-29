f9035fc5f41d2b4f3a65efcc99112ff644af47c0
atulRaaj <atulraj1010@tutanota.com>
2024-08-29 12:45:49 +0530

    updated home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,13 +12,6 @@ export default function Home() {
       <NavBar />
       <div className="mx-4 md:mx-[3.25rem] pt-32 lg:pt-16">
         <HeroSection />
-        {/* 
-        <div className=" hidden md:block md:absolute top-0 left-0 -z-10">
-          <img src="/images/top_left_gradient.png" alt="top left gradient" />
-        </div>
-        <div className="absolute hidden md:block top-0 right-0 -z-10">
-          <img src="/images/top_right_gradient.png" alt="top right gradient" />
-        </div> */}
         {/* <CollabSection /> */}
       </div>
 
