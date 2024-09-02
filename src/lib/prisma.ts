335b81dd8de2752a517f9d6f2c80835764713a75
atulRaaj <atulraj1010@tutanota.com>
2024-09-02 17:34:53 +0530

    API: GST Number api added

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -10,20 +10,6 @@ export const modelMap: Record<string, any> = {
     model: prisma.company,
     attributes: ["id", "name", "address", "created_at", "updated_at"],
   },
-  // user: {
-  //   model: prisma.user,
-  //   attributes: [
-  //     "id",
-  //     "email",
-  //     "password",
-  //     "name",
-  //     "role",
-  //     "companyId",
-  //     "created_at",
-  //     "updated_at",
-  //     "qualityAssurance",
-  //   ],
-  // },
   vendor: {
     model: prisma.vendor,
     attributes: [
@@ -111,21 +97,6 @@ export const modelMap: Record<string, any> = {
     model: prisma.qualityAssurance,
     attributes: ["id", "userId", "created_at", "updated_at"],
   },
-  // rfp: {
-  //   model: prisma.rFP,
-  //   attributes: [
-  //     "id",
-  //     "requirementType",
-  //     "dateOfOrdering",
-  //     "deliveryLocation",
-  //     "deliveryByDate",
-  //     "lastDateToRespond",
-  //     "rfpStatus",
-  //     "userId",
-  //     "created_at",
-  //     "updated_at",
-  //   ],
-  // },
   rfpProduct: {
     model: prisma.rFPProduct,
     attributes: [
