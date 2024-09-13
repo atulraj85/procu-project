1ba66d12b305935991c8e64b7d2156b3d4736ac2
atulRaaj <atulraj1010@tutanota.com>
2024-09-14 03:48:38 +0530

    Backend: Removed int id from generic table

--- src/app/api/[tablename]/route.ts
+++ src/app/api/[tablename]/route.ts
@@ -32,7 +32,7 @@ export async function GET(request: NextRequest) {
               orderByDirection === "asc" ? "asc" : "desc";
           }
         } else if (key === "id") {
-          const ids = value.split(",").map((id) => parseInt(id, 10));
+          const ids = value.split(",").map((id) => id);
           whereClause.id = ids.length > 1 ? { in: ids } : ids[0];
         } else if (key === "state_id") {
           const stateIds = value.split(",").map((id) => parseInt(id, 10));
