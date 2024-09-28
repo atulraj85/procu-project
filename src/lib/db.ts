189f355147abd3e132a11fe3834e9be4e66f3c4c
Ravi Tiwari <rvtwr123@gmail.com>
2024-09-28 09:17:13 +0530

    Disable drizzle's query logs in production.

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,5 +2,10 @@ import * as schema from "@/drizzle/schema";
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const sql = neon(process.env.DATABASE_URL!); // use neon driver instead of pg Pool
-export const db = drizzle(sql, { schema, logger: true });
+export const db = drizzle(sql, {
+  schema,
+  logger: !isProduction,
+});
