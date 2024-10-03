bba96f5d1e69b5d0d9217d250207d901f4a17c74
atulRaaj <atulraj1010@tutanota.com>
2024-10-03 15:22:28 +0530

    Removed tokens unused functions

--- src/lib/token.ts
+++ src/lib/token.ts
@@ -8,63 +8,7 @@ import {
   deletePasswordResetToken,
   findPasswordResetTokenByEmail,
 } from "@/data/password-reset-token";
-import { findUserByEmail } from "@/data/user";
-import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
 import { v4 as uuidv4 } from "uuid";
-
-interface IValidatedToken {
-  userId: number;
-  iat: number;
-  exp: number;
-}
-
-export const generateToken = async (email: string, password: string) => {
-  try {
-    const user = await findUserByEmail(email);
-
-    const passwordMatch = await bcrypt.compare(
-      password,
-      user?.password as string
-    );
-
-    if (!user || !passwordMatch) {
-      throw new Error("Invalid Credentials");
-    }
-
-    return jwt.sign({ userId: user.id }, process.env.JWT_SECRET as string, {
-      expiresIn: "1hr",
-    });
-  } catch (err) {
-    throw new Error((err as any)?.message);
-  }
-};
-
-export const validateToken = async (
-  token: string | null
-): Promise<IValidatedToken | void | null> => {
-  if (!token) {
-    throw new Error("Invalid Token");
-  }
-  try {
-    const decodedToken: IValidatedToken | void = jwt.verify(
-      token.split(" ")[1],
-      process.env.JWT_SECRET as string,
-      function (err, payload) {
-        //@ts-ignore
-        if (err) {
-          throw new Error(err?.message);
-        } else {
-          return payload;
-        }
-      }
-    );
-
-    return decodedToken;
-  } catch (err) {
-    throw new Error((err as any)?.message);
-  }
-};
 
 export async function generateEmailVerificationToken(email: string) {
   const expirationTimeMs = parseInt(
