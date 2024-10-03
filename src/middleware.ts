4b775f20774df1c7b60df93bc63fff31177785cd
Ravi Tiwari <rvtwr123@gmail.com>
2024-10-03 14:58:16 +0530

    Vercel issue is fixed: Remove middleware logs as they are now unnecessary.

--- src/middleware.ts
+++ src/middleware.ts
@@ -20,51 +20,35 @@ export default auth(async (req) => {
   const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
   const isAuthRoute = authRoutes.includes(nextUrl.pathname);
 
-  console.log("Middleware: ", { auth: auth, nextUrl: nextUrl });
-
   if (isApiAuthRoute) {
-    console.log("Middleware: isApiAuthRoute: ", isApiAuthRoute);
     return undefined;
   }
 
   if (isAuthRoute) {
-    console.log("Middleware: isAuthRoute: ", isAuthRoute);
     if (isLoggedIn) {
-      console.log("Middleware: is: isLoggedIn", isLoggedIn);
       return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
     }
     return undefined;
   }
 
-  console.log(
-    `Middleware: isLoggedIn: ${isLoggedIn} isPublicRoute: ${isPublicRoute}`
-  );
   if (!isLoggedIn && !isPublicRoute) {
     return Response.redirect(new URL("/auth/login", nextUrl));
   }
 
   if (isLoggedIn) {
-    console.log(`Middleware: isLoggedIn: ${isLoggedIn}`);
     const token = await getToken({
       req,
       secret: process.env.AUTH_SECRET!,
       secureCookie: true,
     });
-    console.log(`Middleware: token: ${token}`);
     if (!token || !token.role) {
       return NextResponse.redirect(new URL("/auth/login", req.url));
     }
 
     for (const pattern in protectedRoutes) {
-      console.log(
-        `Middleware: pattern: ${pattern} and path: ${nextUrl.pathname}`
-      );
       const regex = new RegExp(pattern);
       if (regex.test(nextUrl.pathname)) {
         const roles = protectedRoutes[pattern];
-        console.log(
-          `Middleware: pattern: ${pattern} and path: ${nextUrl.pathname} and roles: ${roles}`
-        );
         if (!roles.includes(token.role)) {
           return NextResponse.redirect(new URL("/auth/login", req.url));
         }
