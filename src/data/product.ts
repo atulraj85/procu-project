cf89ac7a48d276611c18ef783f901a7e2e1640e2
atulRaaj <atulraj1010@tutanota.com>
2024-10-09 13:41:05 +0530

    Backend: PUT for product

--- src/data/product.ts
+++ src/data/product.ts
@@ -26,3 +26,24 @@ export async function createProduct(data: ProductData) {
     console.error(`Error Creating Product: ${data}`, error);
   }
 }
+
+export async function putProduct(id: string, data: ProductData) {
+  try {
+    const results = await db
+      .update(ProductTable)
+      .set({
+        name: data.name,
+        modelNo: data.modelNo,
+        specification: data.specification,
+        productCategoryId: data.productCategoryId,
+        updatedAt: new Date(),
+      })
+      .where({ id: id}) // Assuming 'id' is the unique identifier for the product
+      .returning();
+
+    return results[0] || null;
+  } catch (error) {
+    console.error(`Error Updating Product: ${data}`, error);
+    throw new Error("Failed to update product"); // Optionally rethrow the error
+  }
+}
